ed2da8862dade888b26252f2ce14766f7f6e5823
Daniel Koman <dankoman30@gmail.com>
2022-06-28 16:31:03 -0700

    prepare interface and menu structure

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 
 using namespace std;
 
-int main() {
-
+void demo() {
 	int TreeKeys[16] = { 24,86,221,46,856,234,3,53,634,13,255,274,2,434,9,22 };
 
 	BST myTree; // create binary search tree object
@@ -47,6 +46,79 @@ int main() {
 			cout << endl;
 		}
 	}
+}
+
+int getNumberFromUser(string message) { // function to get input from user
+	cout << endl << endl << message + ": ";
+	int entry;
+	cin >> entry;
+	while (!cin) { // keep repeating until we have a valid entry
+		cout << endl << "INVALID ENTRY! PLEASE ENTER AN INTEGER!! Try again." << endl;
+		cin.clear(); // clear the input stream
+		cin.ignore(256, '\n');
+		cout << endl << endl << message + ": "; // repeat message
+		cin >> entry;
+	}
+	cout << endl;
+	return entry;
+}
+
+void mainMenu() {
+	for (;;) { // main loop
+
+		BST tree; // instantiate new BST object
+
+		cout << endl << "What would you like to do?" <<
+			"1. Create new tree with specified number of nodes with random values" <<
+			"2. " << endl <<
+			"3. Print the two children of node with specified value" << endl << 
+			"4. Insert a node into the tree" << endl << 
+			"5. Delete a node from the tree" << endl <<
+			"6. Find minimum value in the tree" << endl <<
+			"7. Find maximum value in the tree" << endl << 
+			"8. Traverse the tree and return data in order of traversal" << endl << 
+			"9. Traverse the tree and return data in sorted order" << endl << 
+			"0. EXIT" << endl << endl;
+		int choice = getNumberFromUser("ENTER A CHOICE");
+		switch (choice) {
+		case 1: // new tree with specified number of nodes with random values
+			
+			continue;
+		case 2: // 
+			
+			continue;
+		case 3: // print 2 children of specified node
+			
+			continue;
+		case 4: // insert value into tree
+			tree.AddLeaf(getNumberFromUser("ENTER A VALUE TO ADD TO THE TREE"));
+			continue;
+		case 5: // delete a node
+			
+			continue;
+		case 6: // find minimum
+			
+			continue;
+		case 7: // find maximum
+			
+			continue;
+		case 8: // print in order sorted
+			
+			continue;
+		case 9: // print in order of traversal
+			
+			continue;
+		case 0: // exit
+			cout << "\n\nBYE!!!\n\n";
+			break; // exit loop
+		}
+		break; // exit program
+	}
+}
+
+int main() {
+
+	demo();
 
 	return 0;
 }
